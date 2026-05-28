37b8fe2de427da6ea7802bbf9d5ffa80cb667fe3
wilderjose <wilder.suarez@est.ulsa.edu.ni>
2026-05-28 10:59:45 -0600

    filtro de asistencia

--- src/api/asistencia.js
+++ src/api/asistencia.js
@@ -1,38 +1,42 @@
-import axios from "./axios";
+import api from "./axios";
 
-export const listarAsistencia = async () => {
-  const response = await axios.get("/asistencia/");
-  return response.data;
-};
+export const listarAsistencia = async (fechaInicio, fechaFin = null) => {
+  const params = {};
 
-export const marcarAsistencia = async ({ clase_id, estado, km_inicial = null }) => {
-  const response = await axios.post("/asistencia/marcar/", {
-    clase_id,
-    estado,
-    km_inicial,
+  if (fechaInicio) {
+    params.fecha_inicio = fechaInicio;
+  }
+
+  if (fechaFin) {
+    params.fecha_fin = fechaFin;
+  }
+
+  const response = await api.get("/asistencia/", {
+    params,
   });
 
   return response.data;
 };
 
+export const marcarAsistencia = async (data) => {
+  const response = await api.post("/asistencia/marcar/", data);
+  return response.data;
+};
+
 export const justificarClase = async (asistenciaId, observacion) => {
-  const response = await axios.post(`/asistencia/${asistenciaId}/justificar/`, {
+  const response = await api.post(`/asistencia/${asistenciaId}/justificar/`, {
     observacion,
   });
 
   return response.data;
 };
 
-export const resumenKilometros = async () => {
-  const response = await axios.get("/asistencia/resumen-km/");
+export const finalizarKilometraje = async (data) => {
+  const response = await api.post("/asistencia/finalizar-km/", data);
   return response.data;
 };
 
-export const finalizarKilometraje = async ({ asistencia_id, km_final }) => {
-  const response = await axios.post("/asistencia/finalizar-km/", {
-    asistencia_id,
-    km_final,
-  });
-
+export const resumenKilometros = async () => {
+  const response = await api.get("/asistencia/resumen-km/");
   return response.data;
 };
